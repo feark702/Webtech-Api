ec06862db5688901d7beb3ef044c7733f107e587
Thanakorn Ngamsamer <ngamsamer@hotmail.com>
2017-12-07 17:59:52 +0700

    Add new index.js

--- Project/index.js
+++ Project/index.js
@@ -5,6 +5,25 @@ function isEmptyOrSpaces(str){
     return str === null || str.match(/^ *$/) !== null;
 }
 
-
+var contact1 = [
+    {
+        id:1,
+        First_Name:'Adisak',
+        Last_Name:'Wongkingsri',
+        email: '56660202@go.buu.ac.th'
+    },
+    {
+        id:2,
+        First_Name:'Nattawat',
+        Last_Name:'Viengthong',
+        email: '56660003@go.buu.ac.th'
+    },
+    {
+        id:3,
+        First_Name:'Thanakorn',
+        Last_Name:'Ngamsamer',
+        email: '56660124@go.buu.ac.th'
+    }
+]
 
 module.exports = contacts
